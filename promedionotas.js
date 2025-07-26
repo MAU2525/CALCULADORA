9383882d198ddd5266768ef1c7829947f7f6d828
MAU2525 <161631929+MAU2525@users.noreply.github.com>
2025-07-25 21:06:01 -0500

    Add files via upload

--- promedionotas.js
+++ promedionotas.js
@@ -5,16 +5,19 @@
       n2= parseFloat(document.getElementById("nota2").value);
       n3= parseFloat(document.getElementById("nota3").value);
       n4= parseFloat(document.getElementById("nota4").value);
+      eva= parseFloat(document.getElementById("eva").value);
+
       if(
-        n1== "" || n2=="" || n3=="" || n4=="" ||
+        n1== "" || n2=="" || n3=="" || n4=="" || eva=="" ||
         n1 < 0 || n1 > 20 ||
         n2 < 0 || n2 > 20 ||
         n3 < 0 || n3 > 20 ||
-        n4 < 0 || n4 > 20
+        n4 < 0 || n4 > 20 ||
+        eva < 0 || eva > 20
         ) {
             alert("el campo no puede estar vacio o ingrese notas válidas entre 0 y 20.");
         }
-      prom=Math.round((n1 * 0.20)+(n2 * 0.30)+(n3 * 0.25)+(n4 *0.25 )/4);
+      prom=((n1 * 0.15)+(n2 * 0.15)+(n3 * 0.15)+(n4 *0.15 ) + (eva *0.40));
       
       if (prom>=13) {
         msg=" Aprobaste el curso";
